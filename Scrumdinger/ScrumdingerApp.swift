fa137b86936ffecd99d9b850e5e800b608f3d163
tanu31195 <tanu31195@users.noreply.github.com>
2022-05-27 00:46:11 +0530

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Tanushka Bandara on 2022-05-27.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
